952e8587a5fca7007858003953b717e799c888dd
Sasha Lohozinskyy <0x0sky@proton.me>
2026-08-28 05:49:44 +0300

    Prove explicit Telegram onboarding boundary

--- test/channels/telegram/actor_authorizer_test.rb
+++ test/channels/telegram/actor_authorizer_test.rb
@@ -5,13 +5,12 @@ require_relative "../../test_helper"
 class ActorAuthorizerTest < Minitest::Test
   include PrismBotTestSupport
 
-  def test_maps_telegram_numeric_user_id_to_generic_provider_subject
+  def test_maps_ordinary_telegram_update_to_read_only_actor_resolution
     resolver = FakeActorResolver.new
-    authorizer = PrismBot::Channels::Telegram::ActorAuthorizer.new(
-      resolve_actor: PrismBot::UseCases::ResolveActor.new(actor_resolver: resolver)
-    )
+    onboarder = FakeActorOnboarder.new
+    authorizer = build_authorizer(resolver: resolver, onboarder: onboarder)
 
-    authorized = authorizer.call(telegram_update(user_id: 123_456_789))
+    authorized = authorizer.call(telegram_update(text: "/help", user_id: 123_456_789))
 
     assert_instance_of PrismBot::Channels::Telegram::AuthorizedUpdate, authorized
     assert_equal "person:0x0sky", authorized.actor.canonical_ref
@@ -20,14 +19,46 @@ class ActorAuthorizerTest < Minitest::Test
       [{provider: "telegram", provider_scope: "global", subject_id: "123456789"}],
       resolver.requests
     )
+    assert_empty onboarder.requests
   end
 
-  def test_returns_nil_when_hub_does_not_authorize_the_human_actor
-    resolver = FakeActorResolver.new(denied_subject_ids: [999])
-    authorizer = PrismBot::Channels::Telegram::ActorAuthorizer.new(
-      resolve_actor: PrismBot::UseCases::ResolveActor.new(actor_resolver: resolver)
+  def test_uses_actor_onboarding_only_for_start_command
+    resolver = FakeActorResolver.new
+    onboarder = FakeActorOnboarder.new
+    authorizer = build_authorizer(resolver: resolver, onboarder: onboarder)
+
+    authorized = authorizer.call(
+      telegram_update(text: "/start@PrismBot referral", user_id: 123_456_789)
     )
 
-    assert_nil authorizer.call(telegram_update(user_id: 999))
+    assert_instance_of PrismBot::Channels::Telegram::AuthorizedUpdate, authorized
+    assert_equal "person:0x0sky", authorized.actor.canonical_ref
+    assert_equal(
+      [{provider: "telegram", provider_scope: "global", subject_id: "123456789"}],
+      onboarder.requests
+    )
+    assert_empty resolver.requests
+  end
+
+  def test_unknown_ordinary_actor_never_falls_back_to_onboarding
+    resolver = FakeActorResolver.new(denied_subject_ids: [999])
+    onboarder = FakeActorOnboarder.new
+    authorizer = build_authorizer(resolver: resolver, onboarder: onboarder)
+
+    assert_nil authorizer.call(telegram_update(text: "/publish hello", user_id: 999))
+    assert_equal(
+      [{provider: "telegram", provider_scope: "global", subject_id: "999"}],
+      resolver.requests
+    )
+    assert_empty onboarder.requests
+  end
+
+  private
+
+  def build_authorizer(resolver:, onboarder:)
+    PrismBot::Channels::Telegram::ActorAuthorizer.new(
+      resolve_actor: PrismBot::UseCases::ResolveActor.new(actor_resolver: resolver),
+      onboard_actor: PrismBot::UseCases::OnboardActor.new(actor_onboarder: onboarder)
+    )
   end
 end
