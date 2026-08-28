ede86d4d0e1266ebef6d33e9dbb4d5bc97fdb8ff
Sasha Lohozinskyy <0x0sky@proton.me>
2026-08-28 05:47:59 +0300

    Onboard actors only through Telegram start

--- lib/prism_bot/channels/telegram/actor_authorizer.rb
+++ lib/prism_bot/channels/telegram/actor_authorizer.rb
@@ -6,20 +6,33 @@ module PrismBot
       class ActorAuthorizer
         PROVIDER = "telegram".freeze
         PROVIDER_SCOPE = "global".freeze
+        ONBOARDING_COMMAND = "start".freeze
 
-        def initialize(resolve_actor:)
+        def initialize(resolve_actor:, onboard_actor:)
           @resolve_actor = resolve_actor
+          @onboard_actor = onboard_actor
         end
 
         def call(update)
-          actor = @resolve_actor.call(
+          actor = actor_for(update)
+          return nil unless actor
+
+          AuthorizedUpdate.new(update: update, actor: actor)
+        end
+
+        private
+
+        def actor_for(update)
+          use_case = onboarding_command?(update) ? @onboard_actor : @resolve_actor
+          use_case.call(
             provider: PROVIDER,
             provider_scope: PROVIDER_SCOPE,
             subject_id: update.user_id.to_s
           )
-          return nil unless actor
+        end
 
-          AuthorizedUpdate.new(update: update, actor: actor)
+        def onboarding_command?(update)
+          Command.parse(update.text)&.name == ONBOARDING_COMMAND
         end
       end
     end
