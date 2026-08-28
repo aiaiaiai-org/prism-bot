c7345f9366d827bb88d76bfd56ead03c56c0d5a1
Sasha Lohozinskyy <0x0sky@proton.me>
2026-08-28 05:48:21 +0300

    Add Telegram start handler

---
+++ lib/prism_bot/channels/telegram/handlers/start.rb
@@ -0,0 +1,23 @@
+# © 2026 aiaiaiai · aiaiaiai.org
+
+module PrismBot
+  module Channels
+    module Telegram
+      module Handlers
+        class Start
+          def initialize(message_sender:, presenter:)
+            @message_sender = message_sender
+            @presenter = presenter
+          end
+
+          def call(update:, arguments:)
+            @message_sender.send_message(
+              chat_id: update.chat_id,
+              text: @presenter.started(update.actor)
+            )
+          end
+        end
+      end
+    end
+  end
+end
